fe2359874ad9c44a7bdfbd028cca4605fc8a1dcb
ruggi <ruggiu92@gmail.com>
2019-03-17 17:30:04 +0100

    Versione 0.4 Implementazione provvisoria del Database

--- app/src/main/java/com/example/ruggiu/prova1/WordRoomDatabase.java
+++ app/src/main/java/com/example/ruggiu/prova1/WordRoomDatabase.java
@@ -1,9 +1,12 @@
 package com.example.ruggiu.prova1;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 @Database(entities = {Word.class}, version = 1)
 public abstract class WordRoomDatabase extends RoomDatabase {
@@ -23,5 +26,32 @@ public abstract class WordRoomDatabase extends RoomDatabase {
             }
         }
         return INSTANCE;
+    }
+    private static RoomDatabase.Callback sRoomDatabaseCallback =
+            new RoomDatabase.Callback(){
+
+                @Override
+                public void onOpen (@NonNull SupportSQLiteDatabase db){
+                    super.onOpen(db);
+                    new PopulateDbAsync(INSTANCE).execute();
+                }
+            };
+}
+class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+    private final WordDao mDao;
+
+    PopulateDbAsync(WordRoomDatabase db) {
+        mDao = db.wordDao();
+    }
+
+    @Override
+    protected Void doInBackground(final Void... params) {
+        mDao.deleteAll();
+        Word word = new Word("Hello");
+        mDao.insert(word);
+        word = new Word("World");
+        mDao.insert(word);
+        return null;
     }
 }
